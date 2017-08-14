f48b8ee8b7d8acced8b7ad57e7b49c1b26c11858
cjacquem <cjacquem@student.42.fr>
2017-08-14 23:48:15 +0200

    New version

---
+++ main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import argparse
+import csv
+import random
+
+import settings
+
+
+def import_from_csv(csv_file, season):
+    meals_of_season = []
+    with open(csv_file) as f:
+        f.readline()
+        d = dict(csv.reader(f, delimiter=','))
+        for key, value in d.items():
+            if season == value:
+                meals_of_season.append(key)
+    return meals_of_season
+
+
+def ask_season_to_user():
+    wanted_season = input('Type the wanted season: ').lower()
+    if wanted_season in settings.SPRING:
+        wanted_season = 'spring'
+    elif wanted_season in settings.SUMMER:
+        wanted_season = 'summer'
+    elif wanted_season in settings.AUTUMN:
+        wanted_season = 'autumn'
+    elif wanted_season in settings.WINTER:
+        wanted_season = 'winter'
+    else:
+        print("Your season input is not an available choice.")
+    return wanted_season
+
+
+def random_selection_meals(meals_of_season, wanted_number):
+    week_menu = []
+    count = len(meals_of_season)
+    while len(week_menu) < wanted_number and count:
+        meal = random.choice(meals_of_season)
+        if meal not in week_menu:
+            week_menu.append(meal)
+            count -= 1
+    return week_menu
+
+
+def main():
+    if not args.season:
+        args.season = ask_season_to_user()
+    list_meals = import_from_csv(args.file, args.season)
+    selected_meals = random_selection_meals(list_meals, args.number)
+    if selected_meals:
+        print(selected_meals)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str,
+                        default='dishes.csv',
+                        help="Load your menu file")
+    parser.add_argument("--season", type=str,
+                        choices=["spring", "summer", "autumn", "winter"],
+                        help="Choose your current season")
+    parser.add_argument("--number", type=int,
+                        default=5,
+                        help="Type the number of meals in the generated menu")
+    args = parser.parse_args()
+    main()
